5228737875f14e83eca058a6d2b946180daf8407
mahmoudnassar <mahmoud.nassar@apptcom.com>
2017-05-09 23:01:29 +0200

    Products

--- routes/web.php
+++ routes/web.php
@@ -20,41 +20,42 @@ Route::get('/', 'MainController@home');
 Route::get('/about', 'AboutController@getAbout');
 Route::get('/products', 'ProductController@getProducts');
 Route::get('/products/{id}', 'ProductController@getProduct');
+Route::get('/inquire_order', 'OrderController@order');
+Route::post('/request_order', 'OrderController@requestOrder');
+Route::get('/services', 'ServiceController@getServices');
 
 
 Auth::routes();
 Route::group(['middleware' => ['auth'], 'prefix' => 'admin'], function () {
     Route::get('/home', 'HomeController@index');
 
-Route::get('generator_builder', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@builder');
+    Route::get('generator_builder', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@builder');
 
-Route::get('field_template', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@fieldTemplate');
+    Route::get('field_template', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@fieldTemplate');
 
-Route::post('generator_builder/generate', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@generate');
+    Route::post('generator_builder/generate',
+        '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@generate');
 
-Route::resource('teams', 'TeamController');
+    Route::resource('teams', 'TeamController');
 
 
-Route::resource('projects', 'ProjectController');
+    Route::resource('projects', 'ProjectController');
 
-Route::resource('abouts', 'AboutController');
+    Route::resource('abouts', 'AboutController');
 
-Route::resource('products', 'ProductController');
+    Route::resource('products', 'ProductController');
 
-Route::resource('blogs', 'BlogController');
+    Route::resource('blogs', 'BlogController');
 
-Route::resource('contacts', 'ContactController');
+    Route::resource('contacts', 'ContactController');
 
-Route::resource('orders', 'OrderController');
+    Route::resource('orders', 'OrderController');
 
-Route::resource('quotes', 'QuoteController');
+    Route::resource('quotes', 'QuoteController');
 
-Route::resource('services', 'ServiceController');
+    Route::resource('services', 'ServiceController');
 
-Route::resource('sliders', 'SliderController');
-
+    Route::resource('sliders', 'SliderController');
+});
 // website routes
-Route::get('/inquire_order', 'OrderController@order');
-Route::post('/request_order', 'OrderController@requestOrder');
 
-Route::get('/services', 'ServiceController@getServices');
